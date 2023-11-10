9643b7acdf5a30bb530a27e0888810a7b527b166
HKEDU <HKEDU@DESKTOP-ENFAD1R>
2023-11-10 15:55:59 +0900

    허다빈

--- 17_testgitproject/src/main/java/com/hk/calboard/controller/TestController.java
+++ 17_testgitproject/src/main/java/com/hk/calboard/controller/TestController.java
@@ -2,7 +2,7 @@ package com.hk.calboard.controller;
 
 public class TestController {
 	public String testMain() {
-		System.out.println("이지연한테 사기당함");
+		System.out.println("이지연한테 사기당함 답니 바보 메롱");
 		return "test";
 	}
 }
