6943d35ab02e6372e105207b4851021a375c3f36
HKEDU <HKEDU@DESKTOP-ENFAD1R>
2023-11-13 10:23:38 +0900

    정아

--- 17_testgitproject/src/main/java/com/hk/calboard/controller/TestController.java
+++ 17_testgitproject/src/main/java/com/hk/calboard/controller/TestController.java
@@ -2,7 +2,7 @@ package com.hk.calboard.controller;
 
 public class TestController {
 	public String testMain() {
-		System.out.println("이지연한테 사기당함 답니 바보 메롱 잘 돼 ?");
+		System.out.println("한정아 바보");
 		return "test";
 	}
 }
