fcc9c4ac9b8deb4188626457c3c00ac81f3fca19
HKEDU <HKEDU@DESKTOP-P7H8U5I>
2023-11-13 16:58:04 +0900

    서연 추가

--- 17_testgitproject/src/main/java/com/hk/calboard/controller/TestController.java
+++ 17_testgitproject/src/main/java/com/hk/calboard/controller/TestController.java
@@ -2,7 +2,7 @@ package com.hk.calboard.controller;
 
 public class TestController {
 	public String testMain() {
-		System.out.println("한정아 바보");
+		System.out.println("한정아 바보,바보바보");
 		return "test";
 	}
 }
