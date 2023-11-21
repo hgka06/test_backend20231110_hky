b455088de692e8417a28e6255f465237fca2219b
HKEDU <HKEDU@DESKTOP-ENFAD1R>
2023-11-21 13:50:46 +0900

    h

--- 19_miniproject_hospital/src/main/java/com/hk/board/service/ICalService.java
+++ 19_miniproject_hospital/src/main/java/com/hk/board/service/ICalService.java
@@ -13,21 +13,21 @@ public interface ICalService {
 	
 	//달력생성시 필요한 값 구하는 메서드
 	public Map<String, Integer> makeCalendar(HttpServletRequest request);
-
+	
 	//일정 추가
-	public boolean insertCalBoard(InsertCalCommand insertCalCommand);
+	public boolean insertCalBoard(InsertCalCommand insertCalCommand) ;
 	//일정 목록
-	public List<CalDto> calBoardList(String id, String yyyyMMdd);
+	public List<CalDto> calBoardList(String id,String yyyyMMdd);
 	//일정 상세조회
 	public CalDto calBoardDetail(int seq);
 	//일정 수정하기
 	public boolean calBoardUpdate(UpdateCalCommand updateCalCommand);
 	//일정 삭제하기
 	public boolean calMulDel(Map<String, String[]>map);
-	//한달의 일정 보여주기
-	public List<CalDto> calViewList(String id, String yyyMM);
+	//한달의 일정보여주기
+	public List<CalDto> calViewList(String id, String yyyyMM);
 	//일일의 일정개수 보여주기
-	public int calBoardCount(String id, String yyyyMMdd);
+	public int calBoardCount(String id,String yyyyMMdd);
 }
 
 
